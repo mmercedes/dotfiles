81af80751339c70fa19cd9d23af2c71adee14047
Matt Mercedes <mmercedes@etsy.com>
2018-03-05 00:04:00 +0000

    add oh-my-zsh and plugins

--- install.sh
+++ install.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+if [ -e "$(pwd)/.oh-my-zsh/custom/plugins" ]; then
+    echo "linking ./zsh_plugins to ./.oh-my-zsh/custom/plugins"
+    ln -sf ./zsh_plugins/* ./.oh-my-zsh/custom/plugins/
+fi
+
 # matches all files starting with . followed by alphanumeric char
 for file in \.[[:alnum:]]*
 do
-    if [ "$file" == ".git" ] || [ "$file" == ".gitignore" ]; then
+    if [ "$file" == ".git" ] || [ "$file" == ".gitignore" ] || [ "$file" == ".gitmodules" ]; then
         continue
     fi
 
-    if [ -e "$HOME/$file" ]; then
-        read -p "$HOME/$file already exists. Overwrite with $(pwd)/$file (y/N)? " -n 1 choice
+    if [ ! -L "$HOME/$file" ]; then
+        read -p "$HOME/$file already exists and is NOT a symlink. Overwrite with $(pwd)/$file (y/N)? " -n 1 choice
     else
-        read -p "Symlink $(pwd)/$file to $HOME/$file (y/n)? " -n 1 choice
+        choice="y"
     fi
 
     case $choice in
-        y|Y ) rm -rf "$HOME/$file" && ln -s "$(pwd)/$file" "$HOME/$file" && echo "\n linked $file";;
+        y|Y ) rm -rf "$HOME/$file" && ln -s "$(pwd)/$file" "$HOME/$file" && echo "linked $file";;
         * ) echo "" && continue;;
     esac
 done
